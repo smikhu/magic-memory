23135dd5959946af8de39d3447d0de46c972a668
Steven Mikhu <StevenMikhu@gmail.com>
2022-08-24 23:36:32 -0400

    game fully functional

--- src/App.js
+++ src/App.js
@@ -24,12 +24,15 @@ function App() {
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }));
 
+    setChoiceOne(null)
+    setChoiceTwo(null)
     setCards(shuffledCards);
     setTurns(0);
   };
 
   // handle a choice
   const handleChoice = (card) => {
+    if(card.id === choiceOne?.id) return;
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
   };
 
@@ -65,6 +68,11 @@ function App() {
     setDisabled(false);
   };
 
+  // start a new game automatically
+  useEffect(() => {
+    shuffleCards()
+  }, [])
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
@@ -81,6 +89,7 @@ function App() {
           />
         ))}
       </div>
+      <p>Turns: {turns}</p>
     </div>
   );
 }
